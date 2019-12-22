10d6baf537d91ee4fd81ad3fe2510f9979a739b9
David.Gao <david.alpha.fox@gmail.com>
2019-12-22 14:35:05 +0800

    Add page Render

---
+++ src/aicow_page_handler.erl
@@ -0,0 +1,51 @@
+-module(aicow_page_handler).
+-behaviour(cowboy_middleware).
+
+-ifdef(OTP_RELEASE).
+-compile({nowarn_deprecated_function, [{erlang, get_stacktrace, 0}]}).
+-endif.
+
+-export([execute/2]).
+-export([terminate/5]).
+
+-callback init(Req, any())
+	-> {ok | module(), Req, any()}
+	| {module(), Req, any(), any()}
+	when Req::cowboy_req:req().
+
+-callback terminate(any(), map(), any(),atom()) -> ok.
+-optional_callbacks([terminate/4]).
+
+-spec execute(Req, Env) -> {ok, Req, Env}
+	when Req::cowboy_req:req(), Env::cowboy_middleware:env().
+execute(Req, Env=#{handler := Handler, handler_opts := HandlerOpts, render := Render }) ->
+
+	try Handler:init(Req, HandlerOpts) of
+		{ok, Template,Req2, State} ->
+            Req3 = Render:render(Handler,Template,Req2,State),
+			Result = terminate(normal, Req3, State, Handler,Render),
+			{ok, Req3, Env#{result => Result}};
+        {ok,Template,ContentType,Req2,State}->
+            Req3 = Render:render(Handler,Template,ContentType,Req2,State),
+			Result = terminate(normal, Req3, State, Handler,Render),
+			{ok, Req3, Env#{result => Result}};
+		{Mod, Req2, State} ->
+			Mod:upgrade(Req2, Env, Handler, State);
+		{Mod, Req2, State, Opts} ->
+			Mod:upgrade(Req2, Env, Handler, State, Opts)
+	catch Class:Reason ->
+		StackTrace = erlang:get_stacktrace(),
+		terminate({crash, Class, Reason}, Req, HandlerOpts, Handler,Render),
+		erlang:raise(Class, Reason, StackTrace)
+	end.
+
+terminate(Reason, Req, State, Handler,Render) ->
+	case erlang:function_exported(Handler, terminate, 4) of
+		true -> Handler:terminate(Reason, Req, State,Render);
+		false ->
+            case Reason of
+                normal -> ok;
+                _->
+                    Render:render_exception(Handler,Reason,Req,State)
+            end
+	end.
--- src/aicow_server.erl
+++ src/aicow_server.erl
@@ -1,22 +1,39 @@
 -module(aicow_server).
--export([start/5,start/3]).
+-export([start/5,start/4]).
+-export([start_page/6,start_page/4]).
+
 
 start(Name,Port,Router,Auth,SessionCookie)->
+    Env = env(Router),
+    Env0 = env(Env,Auth,SessionCookie),
+    start(Name,Port,Env0,[cowboy_router,aicow_session_handler,aicow_auth_handler,cowboy_handler]).
+
+start(Name,Port,Env,Middlewares)->
+    case Middlewares of
+        undefined ->
+            cowboy:start_clear(Name,[{port, Port}],#{env => Env});
+        _ ->
+            cowboy:start_clear(Name,[{port, Port}],#{env => Env,middlewares => Middlewares })
+    end.
+
+start_page(Name,Port,Router,Render,Auth,SessionCookie)->
+    Env = env(Router),
+    Env0 = Env#{render => Render},
+    Env1 = env(Env0,Auth,SessionCookie),
+    start(Name,Port,Env1,[cowboy_router,aicow_session_handler,aicow_auth_handler,aicow_page_handler]).
+
+start_page(Name,Port,Router,Render)->
+    Env = env(Router),
+    Env0 = Env#{render => Render},
+    start(Name,Port,Env0,[cowboy_router,aicow_page_handler]).
+
+env(Router)->
     lists:foreach(
       fun({_Path,Module,_Ctx})->
           {module,Module} = code:ensure_loaded(Module)
       end,Router),
     Dispatch = cowboy_router:compile([{'_',Router}]),
-    Env = env(#{dispatch => Dispatch},Auth,SessionCookie),
-    start(Name,Port,Env).
-
-start(Name,Port,Env)->
-    cowboy:start_clear(Name,[{port, Port}],
-                       #{
-                         env => Env,
-                         middlewares => [cowboy_router,aicow_session_handler,
-                                         aicow_auth_handler,cowboy_handler]
-                      }).
+    #{dispatch => Dispatch}.
 
 env(Acc,undefined,undefined) -> Acc;
 env(Acc,Auth,undefined) ->  Acc#{auth => Auth};
