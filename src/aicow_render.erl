5eb57bbb47fc5ac64c4906eeb61d64bbc7454b5a
David.Gao <david.alpha.fox@gmail.com>
2020-01-15 15:35:59 +0800

    Add render behavior

---
+++ src/aicow_render.erl
@@ -0,0 +1,5 @@
+-module(aicow_render).
+-callback render(Handler, Template,Req,State) ->
+    Req when Handler::atom(), Template::binary(), Req::cowboy_req:req(), State::term().
+-callback render_exception(Handler,Reason,Req,State) ->
+    ok when Handler::atom(),Reason::term(), Req::cowboy_req:req(), State::term().
