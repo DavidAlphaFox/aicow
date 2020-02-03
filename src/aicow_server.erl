b18a246bdc286caf637c3876af90a551ca142950
David.Gao <david.alpha.fox@gmail.com>
2020-02-04 01:01:08 +0800

    update aicow_server:start

--- src/aicow_server.erl
+++ src/aicow_server.erl
@@ -1,41 +1,41 @@
 -module(aicow_server).
--export([start/5,start/4]).
--export([start_page/6,start_page/4]).
+-export([start/4]).
 
+-spec start(atom(),integer(),list(),map())-> term().
+start(Name,Port,Router,Config)->
+    Env = env(Router,Config),
+    Middlewares = middlewares(Config),
+    start_clear(Name,Port,Env,Middlewares).
 
-start(Name,Port,Router,Auth,Session)->
-    Env = env(Router),
-    Env0 = env(Env,Auth,Session),
-    start(Name,Port,Env0,[cowboy_router,aicow_session_handler,aicow_auth_handler,cowboy_handler]).
-
-start(Name,Port,Env,Middlewares)->
+start_clear(Name,Port,Env,Middlewares)->
     case Middlewares of
-        undefined ->
-            cowboy:start_clear(Name,[{port, Port}],#{env => Env});
-        _ ->
-            cowboy:start_clear(Name,[{port, Port}],#{env => Env,middlewares => Middlewares })
+      undefined ->
+        cowboy:start_clear(Name,[{port, Port}],#{env => Env});
+      _ ->
+        cowboy:start_clear(Name,[{port, Port}],#{env => Env,middlewares => Middlewares })
     end.
 
-start_page(Name,Port,Router,Render,Auth,Session)->
-    Env = env(Router),
-    Env0 = Env#{render => Render},
-    Env1 = env(Env0,Auth,Session),
-    start(Name,Port,Env1,[cowboy_router,aicow_session_handler,aicow_auth_handler,aicow_page_handler]).
-
-start_page(Name,Port,Router,Render)->
-    Env = env(Router),
-    Env0 = Env#{render => Render},
-    start(Name,Port,Env0,[cowboy_router,aicow_page_handler]).
-
-env(Router)->
+env(Router,Config)->
     lists:foreach(
       fun({_Path,Module,_Ctx})->
           {module,Module} = code:ensure_loaded(Module)
       end,Router),
     Dispatch = cowboy_router:compile([{'_',Router}]),
-    #{dispatch => Dispatch}.
+    case Config of 
+      undefined -> #{dispatch => Dispatch};
+      _ -> Config#{dispatch => Dispatch}
+    end.
 
-env(Acc,undefined,undefined) -> Acc;
-env(Acc,Auth,undefined) ->  Acc#{auth => Auth};
-env(Acc,undefined,Session) ->  Acc#{session => Session};
-env(Acc,Auth,Session) -> Acc#{session=> Session,auth => Auth}.
+middlewares(undefined) -> undefined;
+middlewares(Config)->
+  M0 =
+    case maps:get(render,Config,undefined) of 
+      undefined -> [cowboy_handler];
+      _ -> [aicow_page_handler]
+    end,
+  M1 = middlewares(Config,M0),
+  [cowboy_router|M1].
+middlewares(#{auth := _Auth} = Config,M)-> middlewares(maps:remove(auth,Config),[aicow_auth_handler|M]);
+middlewares(#{session := _Session},M) -> [aicow_session_handler|M];
+middlewares(_,M)-> M.
+
--- src/aicow_session_handler.erl
+++ src/aicow_session_handler.erl
@@ -7,19 +7,19 @@
 
 execute(Req,Env)->
     case get_session(Req,Env) of
-        undefined ->
-            Req0 = set_session(Req,Env),
-            {ok,Req0,Env};
-        Session ->
-            erlang:put(session,Session),
-            {ok,Req,Env}
+      undefined ->
+        Req0 = set_session(Req,Env),
+        {ok,Req0,Env};
+      Session ->
+        erlang:put(aicow_session,Session),
+        {ok,Req,Env}
     end.
 
 
-get_session()-> erlang:get(session).
+get_session()-> erlang:get(aicow_session).
 remove_session(Req)->
     CookieName = erlang:get(cookie_name),
-    erlang:put(session,undefined),
+    erlang:put(aicow_session,undefined),
     cowboy_req:set_resp_cookie(CookieName,<<"">>,Req,#{max_age => 0}).
 
 cookie_name(Env)->
@@ -28,29 +28,24 @@ cookie_name(Env)->
     erlang:put(cookie_name,CookieName),
     CookieName.
 
-cookie_config(Env)->
-    Session = maps:get(session,Env,#{}),
-    CookieConfig = maps:get(cookie_config,Session,?SESSION_COOKIE_CONFIG),
-    erlang:put(cookie_config,CookieConfig),
-    CookieConfig.
 
 get_session(Req,Env) ->
     CookieName = cookie_name(Env),
-    _ = cookie_config(Env),
     try cowboy_req:parse_cookies(Req) of
-        [] -> undefined;
-        Cookies ->
-            SessionID =  proplists:get_value(CookieName,Cookies),
-            erlang:put(session,SessionID),
-            SessionID
+      [] -> undefined;
+      Cookies ->
+        SessionID =  proplists:get_value(CookieName,Cookies),
+        erlang:put(aicow_session,SessionID),
+        SessionID
     catch
         _:_ -> undefined
     end.
 set_session(Req,Env)->
     CookieName = cookie_name(Env),
-    CookieConfig = cookie_config(Env),
+    Session = maps:get(session,Env,#{}),
+    CookieConfig = maps:get(cookie_config,Session,?SESSION_COOKIE_CONFIG),
     Token = token(),
-    erlang:put(session,Token),
+    erlang:put(aicow_session,Token),
     cowboy_req:set_resp_cookie(CookieName,Token, Req,CookieConfig).
 
 
