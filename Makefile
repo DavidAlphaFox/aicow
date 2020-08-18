ba9f633d53d1bf0e43b1f0864716a9f2720fe837
David.Gao <david.alpha.fox@gmail.com>
2020-08-18 10:59:23 +0800

    Add allow-headers on CORS

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 PROJECT = aicow
 PROJECT_DESCRIPTION = Cowboy helper for productions of ailink.io
-PROJECT_VERSION = 0.1.8
+PROJECT_VERSION = 0.1.9
 
 ERLC_OPTS = +debug_info +warn_export_vars +warn_shadow_vars +warn_obsolete_guard -DENABLE_LOG
 
@@ -9,7 +9,7 @@ DEPS = cowboy ailib jiffy
 dep_cowboy_commit = 2.7.0
 
 dep_jiffy = git https://github.com/DavidAlphaFox/jiffy rebar3
-dep_ailib = git https://github.com/DavidAlphaFox/ailib.git v0.4.4
+dep_ailib = git https://github.com/DavidAlphaFox/ailib.git v0.4.5
 
 
 include erlang.mk
--- src/aicow_rest.erl
+++ src/aicow_rest.erl
@@ -1,6 +1,11 @@
 -module(aicow_rest).
+
 -export([options/2,options/3,allow_cors/2]).
 -export([json/4]).
+
+-define(HEADERS,#{<<"access-control-allow-origin">> => <<"*">>,
+                  <<"access-control-allow-headers">> => <<"authorization, content-type">>}).
+  
 
 -spec options(cowboy_req:req(),[atom()]) -> cowboy_req:req().
 options(Req,Actions)-> options(Req,Actions,#{}).
@@ -14,20 +19,15 @@ options(Req,Actions,Headers)->
         Actions0 = lists:map(fun ai_string:to_string/1,Actions),
         ai_string:join(Actions0,",")
     end,
-  DefaultHeaders = #{<<"access-control-allow-methods">> => Actions1,
-                     <<"access-control-allow-origin">> => <<"*">>,
-                     <<"access-control-allow-headers">> => <<"authorization">>},
+  DefaultHeaders = ?HEADERS#{<<"access-control-allow-methods">> => Actions1},
   Headers0 = maps:merge(DefaultHeaders,Headers),
   cowboy_req:set_resp_headers(Headers0,Req).
 
 -spec allow_cors(cowboy_req:req(),undefined|map())-> cowboy_req:req().
 allow_cors(Req,undefined) ->
-  cowboy_req:set_resp_header(<<"access-control-allow-origin">>,
-                             <<"*">>, Req);
+  cowboy_req:set_resp_headers(?HEADERS, Req);
 allow_cors(Req,Headers) ->
-  DefaultHeaders = #{<<"access-control-allow-origin">> => <<"*">>,
-                     <<"access-control-allow-headers">> => <<"authorization">>},
-  Headers0 = maps:merge(DefaultHeaders,Headers),
+  Headers0 = maps:merge(?HEADERS,Headers),
   cowboy_req:set_resp_headers(Headers0,Req).
 
 
